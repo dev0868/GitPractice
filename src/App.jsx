2e7a94060306458dc7a454c0c38acc13438f70d5
dev0869 <devesh@journeyrouters.com>
2024-11-07 17:29:40 +0530

    Price Input Form initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import FilterPage from "./pages/FilterPage";
 import { Route, Routes } from "react-router-dom";
+import CreatePackage from "./pages/Package/CreatePackage";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/Filter" element={<FilterPage />}></Route>
+        <Route path="/packageForm" element={<CreatePackage />}></Route>
       </Routes>
     </div>
   );
